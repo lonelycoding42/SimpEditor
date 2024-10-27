dacc659a64a92ad15ab9083e90f30e6412dcef26
shibei <2939633973@qq.com>
2024-10-27 11:14:59 +0800

    114514

--- src/data.cpp
+++ src/data.cpp
@@ -121,44 +121,45 @@ void distory_list(list_node** top_node){
     return;
 }
 
-int insert(SL_list* ahead_node, SL_list* after_node, char* sentence){
-    SL_list* app_node = (SL_list*)malloc(sizeof(SL_list));
-    app_node->ahead = ahead_node;
-    ahead_node->after = app_node;
+// int insert(SL_list* ahead_node, SL_list* after_node, char* sentence){
+//     SL_list* app_node = (SL_list*)malloc(sizeof(SL_list));
+//     app_node->ahead = ahead_node;
+//     ahead_node->after = app_node;
 
-    app_node->after = after_node;
-    after_node->ahead = app_node;
+//     app_node->after = after_node;
+//     after_node->ahead = app_node;
 
-    strcpy(app_node->sentense, sentence);    
-    return 0;
-}
+//     strcpy(app_node->sentense, sentence);    
+//     return 0;
+// }
 
-int append(SL_list* a_node, char* sentense){
+// int append(SL_list* a_node, char* sentense){
 
-    //确保是在尾部添加
-    SL_list* ahead_node = a_node;
-    while (a_node->after!=nullptr) a_node = a_node->after;
+//     //确保是在尾部添加
+//     SL_list* ahead_node = a_node;
+//     while (a_node->after!=nullptr) a_node = a_node->after;
 
-    SL_list* app_node = (SL_list*)malloc(sizeof(SL_list));
-    app_node->ahead = ahead_node;
-    ahead_node->after = app_node;
+//     SL_list* app_node = (SL_list*)malloc(sizeof(SL_list));
+//     app_node->ahead = ahead_node;
+//     ahead_node->after = app_node;
 
-    app_node->after = nullptr;
-    return 0;
-}
+//     app_node->after = nullptr;
+//     return 0;
+// }
 
-int remove(SL_list* rm_node){
-    //确保链节是最后一个
-    SL_list* node = rm_node;
-    while (node->after!=nullptr) node = node->after;
+// int remove(SL_list* rm_node){
+//     //确保链节是最后一个
+//     SL_list* node = rm_node;
+//     while (node->after!=nullptr) node = node->after;
 
-    node->ahead->after = nullptr;
-    return 0;
-}
+//     node->ahead->after = nullptr;
+//     return 0;
+// }
 
 text_buffer::text_buffer(){
     line = 0;
     char_num = 0;
+    top_node = (list_node **)malloc(SEN_LEN*sizeof(list_node *));
     *top_node = nullptr;
     // *bottom_node = *top_node;
 }
@@ -169,14 +170,17 @@ text_buffer::~text_buffer(){
     return;
 }
 
-void text_buffer::load_file(char* file_name){
+int text_buffer::load_file(char* file_name){
     //这里开销有点大，想想怎么优化
     FILE* fp = fopen(file_name, "r");
+    if ( fp == nullptr) return ERROR;
+
     // list_node** cur = top_node;
-    char buf[SEN_LEN];
+    char buf[SEN_LEN] = {0};
     while (fgets(buf, SEN_LEN, fp)){
         
         // printf("%s", buf);
+        puts(buf);
         node_append(top_node, buf);
         // *cur = (*cur)->after;
         line++;
@@ -184,77 +188,82 @@ void text_buffer::load_file(char* file_name){
     }
         
     // *bottom_node = (*cur);
-    return;
-}
-
-
-int text_buffer::save_file(char* file_name, int mode){
-    FILE* file = fopen(file_name, "w");
-    list_node** node = top_node;
-    while (node!=nullptr)
-    {   
-        // printf("%s", (*node)->sentence);
-        fwrite((*node)->sentence, sizeof((*node)->sentence), 1, file);
-        *node = (*node)->after;
-    }
-    fclose(file);
     return 0;
 }
 
+int text_buffer::save_file(char* file_name){
+    FILE* fp = fopen(file_name, "w");
+    if ( fp == nullptr) return ERROR;
+    
+    list_node** node = top_node;
+    while ((*node)!=nullptr)
+    {   
+        // printf("%s", (*node)->sentence);
+        fwrite((*node)->sentence, sizeof((*node)->sentence), 1, fp);
+        *node = (*node)->after;
+    }
+    fclose(fp);
+    return 1;
+}
+
 //生成前缀函数
-int* prefix_function(char* s) {
-    int n = (int)strlen(s);
-    int  pi[n];
+int_array prefix_function(char* s){
+    int n = strlen(s);
+    int_array  pi{
+        {0}, 0
+    };
+    pi.array_len = n;
+    printf("------\n");
     for (int i = 1; i < n; i++) {
-        int j = pi[i - 1];
-        while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+        int j = pi.array[i - 1];
+        while (j > 0 && s[i] != s[j]) j = pi.array[j - 1];
         if (s[i] == s[j]) j++;
-        pi[i] = j;
+        pi.array[i] = j;
     }
     return pi;
 }
 
 //KMP算法
-int* find_occurrences(char* text, char* pattern) {
-    char* cur;
-    sprintf(cur, "%s%s%s", pattern, '#', text);
-    printf("%s\n", cur);
-    int sz1 = strlen(text), sz2 = strlen(pattern);
-    int* v;
-    int* vp = v;
-    int* lps = prefix_function(cur);
-    for (int i = sz2 + 1; i <= sz1 + sz2; i++){
-        if (lps[i] == sz2){
-            printf("%d\n", i-2*sz2);
-            *(v++) = i-2*sz2;     
-        } 
-    }
-    *v = 0;
-    return vp;
-}
+// int* find_occurrences(char* text, char* pattern) {
+//     char* cur;
+//     sprintf(cur, "%s%s%s", pattern, '#', text);
+//     printf("%s\n", cur);
+//     int sz1 = strlen(text), sz2 = strlen(pattern);
+//     int* v;
+//     int* vp = v;
+//     int* lps = prefix_function(cur);
+//     for (int i = sz2 + 1; i <= sz1 + sz2; i++){
+//         if (lps[i] == sz2){
+//             printf("%d\n", i-2*sz2);
+//             *(v++) = i-2*sz2;     
+//         } 
+//     }
+//     *v = 0;
+//     return vp;
+// }
 
-pos text_buffer::seek_phrase(char* phrase, SL_list* node){
-    // SL_list* node = top_sentense;
-    // pos pos_array;
-    // for (int i = 0; i<line; i++)
-    // {
-    //     pos_array[i].line = node;
-    //     pos_array[i].char_pos = find_occurrences(node->sentense, phrase);
-    //     node = node->after;
-    // }
-    // return pos_array;
-    pos pos_info;
-    pos_info.line = node;
-    pos_info.char_pos = find_occurrences(node->sentense, phrase);
-    return pos_info;
-}
+// pos text_buffer::seek_phrase(char* phrase, SL_list* node){
+//     // SL_list* node = top_sentense;
+//     // pos pos_array;
+//     // for (int i = 0; i<line; i++)
+//     // {
+//     //     pos_array[i].line = node;
+//     //     pos_array[i].char_pos = find_occurrences(node->sentense, phrase);
+//     //     node = node->after;
+//     // }
+//     // return pos_array;
+//     pos pos_info;
+//     pos_info.line = node;
+//     pos_info.char_pos = find_occurrences(node->sentense, phrase);
+//     return pos_info;
+// }
 
-bool text_buffer::rpls_phrase(char* r_phrase, char* rd_phrase, SL_list* node, int char_pos){
-    // pos mypos = seek_phrase(rd_phrase, node);
-    char* temp;
-    strncpy(temp, node->sentense, char_pos-1);
-    sprintf(temp, "%d%d%d", temp, r_phrase, 
-            node->sentense+char_pos+strlen(rd_phrase));
-    strcpy(node->sentense, temp);
-    return true;
-}
+// bool text_buffer::rpls_phrase(char* r_phrase, char* rd_phrase, SL_list* node, int char_pos){
+//     // pos mypos = seek_phrase(rd_phrase, node);
+//     char* temp;
+//     strncpy(temp, node->sentense, char_pos-1);
+//     sprintf(temp, "%d%d%d", temp, r_phrase, 
+//             node->sentense+char_pos+strlen(rd_phrase));
+//     strcpy(node->sentense, temp);
+//     return true;
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,20 @@
 
 int main( )
 {
-    char test1[100] = "abcdabcd";
+    char test1[100] = "abcdabcd\r\n";
     char test2[100] = "ab";
-    printf("%s\n%s\n\n", test1, test2);
+    // printf("%s\n%s\n\n", test1, test2);
 
-    text_buffer myTEXT;
-    myTEXT.load_file("test.txt");
-    myTEXT.save_file("test.txt", SAVE);
+    // text_buffer myTEXT;
+    int_array my_arr =  prefix_function(test1);
+    for (int i = 0; i < my_arr.array_len; i++)
+    {
+        printf("%d ", my_arr.array[i]);
+    }
+    
+    // myTEXT.load_file("test");
+    // myTEXT.save_file("test");
+
     // int* arr = find_occurrences(test1, test2);
     // for (int i = 0; i < 2; i++)
     // {
